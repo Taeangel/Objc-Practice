064804e111591125e276e092d7a12be51e6976ac
Taeangel <wngks2050@gmail.com>
2023-03-29 00:50:58 +0900

    objc bool

--- ObjcPractice/ObjcPractice/ViewController.m
+++ ObjcPractice/ObjcPractice/ViewController.m
@@ -16,7 +16,17 @@
 - (void)viewDidLoad {
   [super viewDidLoad];
   // Do any additional setup after loading the view.
+  
+  BOOL isDark = YES;
+  BOOL isLight = NO;
+
+  if (!isDark) {
+    NSLog(@"다크모드입니다!? %@", isDark ? @"TRUE" : @"False");
+  } else {
+    NSLog(@"라이트모드입니다!? %@", isLight ? @"TRUE" : @"False");
+  }
 }
+
 
 
 @end
