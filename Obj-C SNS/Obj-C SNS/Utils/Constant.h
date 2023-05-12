30c041bcb829cf6203e7378ae853bfd0f562010f
Taeangel <wngks2050@gmail.com>
2023-05-13 00:10:53 +0900

    feat: PostList constant 구현

---
+++ Obj-C SNS/Obj-C SNS/Utils/Constant.h
@@ -0,0 +1,16 @@
+//
+//  Constant.h
+//  Obj-C SNS
+//
+//  Created by song on 2023/05/12.
+//
+
+#ifndef Constant_h
+#define Constant_h
+#import <Foundation/Foundation.h>
+// MARK: notification names
+
+FOUNDATION_EXPORT NSString * const PostListVCShouldFetchListNotification;
+
+
+#endif /* Constant_h */
---
+++ Obj-C SNS/Obj-C SNS/Utils/constants.m
@@ -0,0 +1,11 @@
+//
+//  constants.m
+//  Obj-C SNS
+//
+//  Created by song on 2023/05/12.
+//
+# import "Constant.h"
+
+// 포스트 리스트 갱신 데이터 이벤트
+FOUNDATION_EXPORT NSString * const PostListVCShouldFetchListNotification = @"PostListVCShouldFetchListNotification";
+
