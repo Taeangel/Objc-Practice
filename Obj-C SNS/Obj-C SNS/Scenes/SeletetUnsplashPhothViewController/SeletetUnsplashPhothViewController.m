65c79fd13ae827bc7baad4059d9de73056ff9898
Taeangel <wngks2050@gmail.com>
2023-05-13 01:25:33 +0900

    feat: searcbar 디바운스 처리

--- Obj-C SNS/Obj-C SNS/Scenes/SeletetUnsplashPhothViewController/SeletetUnsplashPhothViewController.m
+++ Obj-C SNS/Obj-C SNS/Scenes/SeletetUnsplashPhothViewController/SeletetUnsplashPhothViewController.m
@@ -9,7 +9,7 @@
 
 @interface SeletetUnsplashPhothViewController ()
 @property (weak, nonatomic) IBOutlet UISearchBar *photoSearchBar;
-
+@property (strong, nullable) dispatch_block_t debounceSearchInputTask;
 @end
 
 @implementation SeletetUnsplashPhothViewController
@@ -31,9 +31,27 @@
 
 
 - (void)searchBar:(UISearchBar *)searchBar textDidChange:(NSString *)searchText {
-  NSLog(@"%s , line: %d, %@", __func__, __LINE__, searchText);
+//  NSLog(@"%s , line: %d, %@", __func__, __LINE__, searchText);
   
   
+  //글자 입력시 디바운스 작업 초기화
+  if (_debounceSearchInputTask != nil) {
+    dispatch_block_cancel(_debounceSearchInputTask);
+    _debounceSearchInputTask = nil;
+  }
+  
+  if (searchText.length < 1) {
+    return;
+  }
+  
+  dispatch_block_t task = dispatch_block_create_with_qos_class(DISPATCH_BLOCK_ENFORCE_QOS_CLASS, QOS_CLASS_USER_INITIATED, 0, ^{
+    NSLog(@"%s , line: %d,디바운스된 글자 %@", __func__, __LINE__, searchText);
+  });
+  
+  _debounceSearchInputTask = task;
+  
+  dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 0.7 * NSEC_PER_SEC), dispatch_get_main_queue(), task);
+   
 }
 
 
