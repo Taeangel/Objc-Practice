7a664c97cce0ccf7e2eb8348b893cd7698b21edb
Taeangel <wngks2050@gmail.com>
2023-05-12 13:41:51 +0900

    feat: Viewcontroller 생성

---
+++ Obj-C SNS/Obj-C SNS/Scenes/AddPost/AddPostViewController.h
@@ -0,0 +1,16 @@
+//
+//  AddPostViewController.h
+//  Obj-C SNS
+//
+//  Created by song on 2023/05/12.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface AddPostViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Obj-C SNS/Obj-C SNS/Scenes/AddPost/AddPostViewController.m
@@ -0,0 +1,31 @@
+//
+//  AddPostViewController.m
+//  Obj-C SNS
+//
+//  Created by song on 2023/05/12.
+//
+
+#import "AddPostViewController.h"
+
+@interface AddPostViewController ()
+
+@end
+
+@implementation AddPostViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
