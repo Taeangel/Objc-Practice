43dc4f79435524efd34a65bacc88161b9e165603
Taeangel <wngks2050@gmail.com>
2023-05-15 12:12:10 +0900

    feat: 이미지 포함 creat 구헌

--- Obj-C SNS/Obj-C SNS/Scenes/AddPost/AddPostViewController.m
+++ Obj-C SNS/Obj-C SNS/Scenes/AddPost/AddPostViewController.m
@@ -7,7 +7,9 @@
 
 #import "AddPostViewController.h"
 
-@interface AddPostViewController ()
+@interface AddPostViewController () {
+  NSString * _selectedImgUrlString;
+}
 
 @property (weak, nonatomic, nullable) FIRFirestore * db;
 
@@ -20,7 +22,7 @@
 @implementation AddPostViewController
 
 - (void)viewDidLoad {
-    [super viewDidLoad];
+  [super viewDidLoad];
   _db = [FIRFirestore firestore];
   
   _postImageView.sd_imageTransition = SDWebImageTransition.fadeTransition;
@@ -44,27 +46,28 @@
   
   [self addPostWithTitle:titleInput
              withContent:contentInput
+            withImageUrl: _selectedImgUrlString ? : @""
           withCompletion:^{
     NSLog(@"%s , line: %d, %@", __func__, __LINE__, @"컴플리션 완료");
     AddPostViewController * strongSelf = weakSelf;
     if (strongSelf) {
       [strongSelf dismissViewControllerAnimated:YES completion:nil];
     }
-}];
+  }];
 }
 
 - (IBAction)onPhotoSelectBtnClicked:(UIButton *)sender {
   NSLog(@"%s , line: %d, %@", __func__, __LINE__, @"사진선택 클릭");
   
   __weak AddPostViewController * weakSelf = self;
-
- SeletetUnsplashPhothViewController * selectPhotoVC = (SeletetUnsplashPhothViewController *) [SeletetUnsplashPhothViewController presentWithNavigationAndReturnVC:self];
+  
+  SeletetUnsplashPhothViewController * selectPhotoVC = (SeletetUnsplashPhothViewController *) [SeletetUnsplashPhothViewController presentWithNavigationAndReturnVC:self];
   selectPhotoVC.photoSelectionBlock = ^(NSString * selectedUrl) {
     NSLog(@"%s , line: %d, %@", __func__, __LINE__, selectedUrl);
     AddPostViewController * strongSelf = weakSelf;
     if (strongSelf) {
       NSURL * imgUrl = [[NSURL alloc] initWithString: selectedUrl];
-      
+      strongSelf->_selectedImgUrlString = selectedUrl;
       [strongSelf->_postImageView sd_setImageWithURL:imgUrl];
     }
   };
@@ -73,6 +76,7 @@
 // MARK: 인스턴스 메서드
 -(void) addPostWithTitle: (NSString * )title
              withContent: (NSString *) content
+            withImageUrl: (NSString *) imageUrlString
           withCompletion: (void(^)(void)) completion// 첫번째 보이드가 반환 두번쨰 보이드가 매개변수
 {
   FIRDocumentReference * newPostRef = [[self.db collectionWithPath:@"posts"] documentWithAutoID];
@@ -81,7 +85,7 @@
     @"identifier": newPostRef.documentID,
     @"title": title,
     @"content": content,
-    @"image": @"https://images.unsplash.com/photo-1675879102146-810c95632b5b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw0NXx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60",
+    @"image": imageUrlString,
     @"created_at": [FIRTimestamp timestampWithDate:[NSDate date]],
     @"updated_at": [FIRTimestamp timestampWithDate:[NSDate date]],
   };
@@ -95,7 +99,7 @@
       
       [[NSNotificationCenter defaultCenter]postNotificationName:PostListVCShouldFetchListNotification object:self];
       completion();
-    } 
+    }
   }];
   
 }
--- Obj-C SNS/Obj-C SNS/Scenes/SeletetUnsplashPhothViewController/SeletetUnsplashPhothViewController.m
+++ Obj-C SNS/Obj-C SNS/Scenes/SeletetUnsplashPhothViewController/SeletetUnsplashPhothViewController.m
@@ -57,6 +57,17 @@
   
   if (_photoSelectionBlock == nil) { return; }
   
+  if (_selectedImgURLString == nil) {
+    UIAlertController* alert = [UIAlertController alertControllerWithTitle:@"안내" message:@"이미지를 선택하세요" preferredStyle:UIAlertControllerStyleAlert];
+    
+    UIAlertAction* defaultAction = [UIAlertAction actionWithTitle:@"확인" style:UIAlertActionStyleDefault handler:^(UIAlertAction * action) {}];
+    [alert addAction:defaultAction];
+    
+    [self presentViewController:alert animated:YES completion:nil];
+    
+    return; 
+  }
+  
   _photoSelectionBlock(_selectedImgURLString);
   
   [self dismissViewControllerAnimated:YES completion:nil];
