205cee1c9458a745cea013ff0e561800bacafa9e
Taeangel <wngks2050@gmail.com>
2023-05-15 20:51:22 +0900

    feat: detailView data 넣어주기

--- Obj-C SNS/Obj-C SNS/Scenes/DetailSNS/DetailSNSViewController.h
+++ Obj-C SNS/Obj-C SNS/Scenes/DetailSNS/DetailSNSViewController.h
@@ -7,6 +7,8 @@
 
 #import <UIKit/UIKit.h>
 #import "Post.h"
+#import "NSDate+Helpers.h"
+
 
 NS_ASSUME_NONNULL_BEGIN
 
--- Obj-C SNS/Obj-C SNS/Scenes/DetailSNS/DetailSNSViewController.m
+++ Obj-C SNS/Obj-C SNS/Scenes/DetailSNS/DetailSNSViewController.m
@@ -6,10 +6,16 @@
 //
 
 #import "DetailSNSViewController.h"
+@import SDWebImage;
 
 @interface DetailSNSViewController () {
   Post * _post;
 }
+@property (weak, nonatomic) IBOutlet UILabel *postId;
+@property (weak, nonatomic) IBOutlet UILabel *postTitle;
+@property (weak, nonatomic) IBOutlet UILabel *postContent;
+@property (weak, nonatomic) IBOutlet UILabel *postCreatedAt;
+@property (weak, nonatomic) IBOutlet UIImageView *postImageView;
 
 @end
 
@@ -22,7 +28,17 @@
 
 -(void) initSetting {
   _post = _interfacePost;
+  
+  [_postImageView sd_setImageWithURL:[NSURL URLWithString:_post.image]
+               placeholderImage:[UIImage systemImageNamed:@"photo.artframe"]];
+  
+  _postId.text = [@"id: " stringByAppendingString: _post.identifier];
+  _postTitle.text = [@"title: " stringByAppendingString: _post.title];
+  _postContent.text = [@"content: " stringByAppendingString: _post.content];
+  _postCreatedAt.text = [@"createdAt: " stringByAppendingString: [_post.createdAt toString]];
+  
   self.navigationItem.title = _post.title;
+  
 }
 
 - (IBAction)onDeleteBtnClicked:(id)sender {
@@ -33,4 +49,6 @@
 - (IBAction)onModifyPostBtnClicked:(id)sender {
   
 }
+
+
 @end
