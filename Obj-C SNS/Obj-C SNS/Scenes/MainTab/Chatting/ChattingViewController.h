09da590116f080f9a45710d71e2d9e1749f3fa84
Taeangel <wngks2050@gmail.com>
2023-05-20 23:59:50 +0900

    feat: chattingView 연결

---
+++ Obj-C SNS/Obj-C SNS/Scenes/MainTab/Chatting/ChattingViewController.h
@@ -0,0 +1,16 @@
+//
+//  ChattingViewController.h
+//  Obj-C SNS
+//
+//  Created by song on 2023/05/20.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ChattingViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
